fb2e06fdfb0252a865e42e49eb1c021aad7b9d59
undersfx <atchin_@hotmail.com>
2021-02-25 20:57:48 -0300

    add: Revisão de Solução

--- aula_02/desafio.py
+++ aula_02/desafio.py
@@ -16,8 +16,8 @@ from math import inf
 
 def for_loop_minmax(lista: list) -> tuple:
     """
-    Implementação iterativa comparando todos elementos da lista com os valores
-    de maior e menor armazenados anteriormente.
+    Implementação iterativa comparando todos elementos da lista com
+    os valores de maior e menor armazenados anteriormente.
 
     Doctests:
     >>> for_loop_minmax([0, 1, 2, 3])
@@ -48,7 +48,7 @@ def for_loop_minmax(lista: list) -> tuple:
     maior = -inf
 
     if not lista:
-        raise ValueError
+        raise ValueError('Lista vazia')
 
     for item in lista:
         if item < menor:
@@ -60,10 +60,9 @@ def for_loop_minmax(lista: list) -> tuple:
     return (menor, maior)
 
 
-def recursive_minmax(lista: list) -> tuple:
+def recursive_minmax(lista) -> tuple:
     """
-    Implementação recursiva deixando todas as comparações na pilha de chamadas
-    até alcançar a mínima lista para resolver a comparação dos elementos
+    Implementação recursiva transformando o objeto lista em um iterador.
 
     Doctests:
     >>> recursive_minmax([0, 1, 2, 3])
@@ -77,45 +76,36 @@ def recursive_minmax(lista: list) -> tuple:
 
     Complexidade de Tempo:
     2n comparações      -> f(n) = 2n -> n
-    2n slicing          -> f(n) = 2n -> n
-    2n+2 atribuições    -> f(n) = 2n + 2 -> n
-    2n+1 decisão        -> f(n) = 2n + 1 -> n
+    2n+1 atribuições    -> f(n) = 2n + 1 -> n
+    2n+1 decisões        -> f(n) = 2n + 1 -> n
 
-    Total: f(n) = 4n -> O(n)
+    Total: f(n) = 3n -> O(n)
 
 
     Complexidade de Espaço:
-    2 funções           -> f(n) = c -> 1
-    2n variaveis (list) -> f(n) = 2n -> n
-    n^2 call stack      -> f(n) = n^2 -> n^2
+    1 função        -> f(n) = c -> 1
+    1 iterador      -> f(n) = c -> 1
+    n call stack    -> f(n) = n -> n
 
-    Total: f(n) = 1 + n + n^2 -> O(n^2)
+    Total: f(n) = 2 + n -> O(n)
     """
-    def recursive_menor(lista: list) -> int:
-        primeiro = lista[0]
-
-        if tamanho(lista) == 1:
-            return primeiro
+    def _recursive_minmax(iteravel, _min, _max):
+        try:
+            elemento = next(iteravel)
+        except StopIteration:
+            return _min, _max
         else:
-            menor = recursive_menor(lista[1:])
-            return menor if menor < primeiro else primeiro
+            if elemento < _min:
+                _min = elemento
+            if elemento > _max:
+                _max = elemento
+            return _recursive_minmax(iteravel, _min, _max)
 
 
-    def recursive_maior(lista: list) -> int:
-        primeiro = lista[0]
-
-        if tamanho(lista) == 1:
-            return primeiro
-        else:
-            maior = recursive_maior(lista[1:])
-            return maior if maior > primeiro else primeiro
-
-
-    if not lista:
-        raise ValueError
-
-    _menor = recursive_menor(lista)
-    _maior = recursive_maior(lista)
+    iteravel = iter(lista)
+    _menor, _maior = _recursive_minmax(iteravel, inf, -inf)
+    if _menor is inf:
+        raise ValueError('Lista vazia')
 
     return (_menor, _maior)
 
@@ -136,87 +126,39 @@ def tail_recursive_minmax(lista: list) -> tuple:
     (0, 0)
 
     Complexidade de Tempo:
-    2n slicing          -> f(n) = 2n -> n
     2n comparações      -> f(n) = 2n -> n
-    2n+2 atribuições    -> f(n) = 2n + 2 -> n
-    2n+1 decisão        -> f(n) = 2n + 1 -> n
+    4n atribuições      -> f(n) = 4n -> n
+    3n decisões         -> f(n) = 3n -> n
 
-    Total: f(n) = 4n -> O(n)
+    Total: f(n) = 3n -> O(n)
 
 
     Complexidade de Espaço:
-    2 funções           -> f(n) = c -> 1
+    1 função            -> f(n) = c -> 1
+    4n atribuições      -> f(n) = 4c -> 1
     2 variaveis (int)   -> f(n) = c -> 1
-    1 variavel (list)   -> f(n) = n - 1 -> n
 
-    Total: f(n) = 2 + n -> O(n)
+    Total: f(n) = 2 + n -> O(1)
     """
-    def tail_recursive_menor(lista: list, tail=inf) -> int:
-        primeiro = lista[0]
+    def _tail_recursive_minmax(lista, pos, _min, _max):
+        if pos == len(lista):
+            return _min, _max
 
-        if primeiro < tail:
-            tail = primeiro
+        i = lista[pos]
 
-        if tamanho(lista) == 1:
-            return tail
+        if i < _min:
+            _min = i
 
-        return tail_recursive_menor(lista[1:], tail)
+        if i > _max:
+            _max = i
 
+        pos += 1
+        return _tail_recursive_minmax(lista, pos, _min, _max)
 
-    def tail_recursive_maior(lista: list, tail=-inf) -> int:
-        primeiro = lista[0]
-
-        if primeiro > tail:
-            tail = primeiro
-
-        if tamanho(lista) == 1:
-            return tail
-
-        return tail_recursive_maior(lista[1:], tail)
-
-    if not lista:
-        raise ValueError
-
-    _menor = tail_recursive_menor(lista)
-    _maior = tail_recursive_maior(lista)
-
-    return (_menor, _maior)
+    return _tail_recursive_minmax(lista, 0, inf, -inf)
 
 
-# Helper Functions ------------------------------------------
-def tamanho(iteravel):
-    '''
-    >>> tamanho([1, 2, 3])
-    3
-
-    >>> tamanho([1])
-    1
-
-    >>> tamanho([])
-    0
-
-    Complexidade de Tempo:
-    n atribuições   -> f(n) = c -> 1
-    1 descisão      -> f(n) = c -> 1
-    1 laço          -> f(n) = n -> n
-
-    Total: f(n) = 2 + n -> O(n)
-
-
-    Complexidade de Espaço:
-    1 variavel (int) -> f(n) = c -> 1
-
-    Total: f(n) = 1 -> O(1)
-    '''
-    i = 0
-    if not iteravel:
-        return 0
-
-    while True:
-        try:
-            iteravel[i]
-        except IndexError:
-            break
-        i += 1
-
-    return i
+if __name__ == '__main__':
+    for_loop_minmax([0, 1, 2, 3])
+    recursive_minmax([0, 1, 2, 3])
+    tail_recursive_minmax([0, 1, 2, 3])
