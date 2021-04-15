9c3740cb97526bc9d5c5ee69f6c9181fc3954fba
undersfx <atchin_@hotmail.com>
2021-04-15 17:44:00 -0300

    wip: avaliacao aritimetica

--- aula_05/avaliacao_expressao_aritmetica.py
+++ aula_05/avaliacao_expressao_aritmetica.py
@@ -5,6 +5,7 @@
 from numbers import Number
 
 from aula_05.fila import Fila
+from aula_04.pilha_com_lista_duplamente_ligada import Pilha, PilhaVaziaExcecao
 
 
 class FilaHelper(Fila):
@@ -16,6 +17,17 @@ class FilaHelper(Fila):
         else:
             num = int(num)
         return num
+
+    def sanitizar(self) -> Fila:
+        fila_sanitizada = Fila()
+        for token in self._deque:
+            if token in ('[', '{'):
+                fila_sanitizada.enfileirar('(')
+            elif token in (']', '}'):
+                fila_sanitizada.enfileirar(')')
+            else:
+                fila_sanitizada.enfileirar(token)
+        return fila_sanitizada
 
 
 Fila = FilaHelper
@@ -35,6 +47,9 @@ def analise_lexica(expressao:str) -> Fila:
     e verificar se demais caracteres são validos: +-*/(){}[]
     :param expressao: string com expressao a ser analisada
     :return: fila com tokens
+
+    Time Complexity = O(n)
+    Space Complexity = O(n)
     """
     fila_lexica = Fila()
     temp = Fila()
@@ -69,11 +84,14 @@ def analise_sintatica(fila:Fila) -> Fila:
 
     :param fila: fila proveniente de análise lexica
     :return: fila_sintatica com elementos tokens de numeros
+
+    Time Complexity = O(n)
+    Space Complexity = O(n)
     """
     if not fila:
         raise ErroSintatico()
 
-    result = Fila()
+    fila_sintatica = Fila()
     temp = Fila()
     caracteres_validos = ("+", "-", "*", "/", "(", ")", "{", "}", "[", "]")
 
@@ -83,16 +101,67 @@ def analise_sintatica(fila:Fila) -> Fila:
             continue
 
         if token in caracteres_validos:
-            if temp: result.enfileirar(temp.to_number())
+            if temp: fila_sintatica.enfileirar(temp.to_number())
 
-            result.enfileirar(token)
+            fila_sintatica.enfileirar(token)
             continue
 
         raise ErroSintatico()
     else:
-        if temp: result.enfileirar(temp.to_number())
+        if temp: fila_sintatica.enfileirar(temp.to_number())
 
-    return result
+    return fila_sintatica
+
+
+def _avaliar_operacao(n: Number, operation: str, m: Number) -> Number:
+    """
+    Avalia uma operação matemática e retorno o resultado.
+
+    >>> _avaliar_operacao(1, "+", 2)
+    3
+
+    Time Complexity = O(1)
+    Space Complexity = O(1)
+    """
+    case = {
+        '+': lambda n, m: n + m,
+        '-': lambda n, m: n - m,
+        '*': lambda n, m: n * m,
+        '/': lambda n, m: n / m,
+    }
+    try:
+        return case[operation](n, m)
+    except TypeError as e:  
+        raise ValueError('Operação não suportada') from e
+
+
+def _resolver_expressao_flat(expressao: Pilha) -> Number:
+    """
+    Avalia uma Pilha normalizada de operações sequenciais sem considerar precedência.
+
+    >>> pilha = Pilha()
+    >>> pilha._deque.extend([2, '*', 2, '/', 1, '+', 1])
+    >>> _resolver_expressao_flat(pilha)  # 1 + 1 / 2 * 2
+    2
+
+    Time Complexity = O(n)
+    Space Complexity = O(1)
+    """
+    n = expressao.desempilhar()
+    try:
+        operacao = expressao.desempilhar()
+        m = expressao.desempilhar()
+    except PilhaVaziaExcecao:
+        return n
+
+    resultado = _avaliar_operacao(n, operacao, m)
+
+    while not expressao.esta_vazia:
+        operacao = expressao.desempilhar()
+        m = expressao.desempilhar()
+        resultado = _avaliar_operacao(resultado, operacao, m)
+
+    return resultado
 
 
 def avaliar(expressao:str) -> Number:
@@ -100,8 +169,34 @@ def avaliar(expressao:str) -> Number:
     Função que avalia expressão aritmetica retornando seu valor se não houver nenhum erro
     :param expressao: string com expressão aritmética
     :return: valor númerico com resultado
+
+    Time Complexity = O(n**2)
+    Space Complexity = O(n)
     """
     # Fazer análise léxica
     # Fazer análise sintática
     # Processar os tokens gerados
-    pass
+
+    expressao = analise_lexica(expressao)
+    expressao = analise_sintatica(expressao)
+    expressao = expressao.sanitizar()
+    
+    pilha_de_execucao = Pilha()
+    temp = Pilha()
+
+    for token in expressao:
+        if token != ')':
+            pilha_de_execucao.empilhar(token)
+        else:
+            token = pilha_de_execucao.desempilhar()
+
+            while token != '(':
+                temp.empilhar(token)
+                token = pilha_de_execucao.desempilhar()
+
+            pilha_de_execucao.empilhar(_resolver_expressao_flat(temp))
+            temp = Pilha()
+
+    resultado = _resolver_expressao_flat(pilha_de_execucao)
+
+    return resultado
